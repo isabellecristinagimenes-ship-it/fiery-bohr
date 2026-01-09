a067e2dfdddfa5420a319e30fc6c70c170e285d8
Isabelle Cristina Gimenes <isabellecristinagimenes@MacBook-Air-de-Isabelle-2.local>
2026-01-09 16:54:47 -0300

    feat: enable CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const metricsRoutes = require('./routes/metricsRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
+app.use(cors());
 app.use(express.json());
 app.use('/metrics', metricsRoutes);
 
